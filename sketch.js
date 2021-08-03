8d9ec30fd4a136995cc49a200acc6df84e87f38f
Justin <justinlema0012@gmail.com>
2021-08-03 19:23:01 -0400

    Css installed

--- sketch.js
+++ sketch.js
@@ -2,6 +2,14 @@
 let todoList = [];
 // our todo list is an empty array
 
+// random function
+function random(max) {
+    let randDec = Math.random() * (max + 1);
+    let randInt = Math.floor(randDec);
+    console.log(randInt);
+    return randInt;
+}
+
 // grab our button from our document
 let addButton = document.querySelector("#addB");
 let removeButton = document.querySelector("#removeB")
@@ -17,13 +25,24 @@ let numberInputText = document.querySelector("#numInput");
 addButton.onclick = function(event) {
 // prevent refreshing
 event.preventDefault();
+    // random colors 
+    let r = random(255);
+    let g = random(255);
+    let b = random(255);
 
     const string = inputText.value;
     // create our additional tag for the additional row
     let additionalRow = document.createElement('li');
     // add the content to that row
-    // below we add the text for our todo list!
+    // below we add the text for our todo list
     additionalRow.append(string);
+
+    // style each row for todoList
+    additionalRow.style.fontSize = "18px";
+    additionalRow.style.borderBottom = "grey solid 2px";
+    additionalRow.style.borderTop = "grey solid 2px";
+    additionalRow.style.marginBottom = "5px";
+    additionalRow.style.color = "rgb("+r+","+g+","+b+")";
     // append our additional row in the list into the DOM, we add the element in the parent node 'ol' at the end
     list.append(additionalRow);
     todoList.push(string);
